04d320d8fc7fe8f5fcdb16090f7f646ae63f2fd2
jacob.ruokojarvi <jacob.ruokojarvi@elev.ga.ntig.se>
2019-10-24 11:16:38 +0200

    start

---
+++ Code/PRG/Ekvationslosning/src/ekvationslösning.java
@@ -0,0 +1,9 @@
+import
+
+import javax.swing.*;.javax.swing.*;
+
+public class ekvationslösning {
+        public static void main(String[] args) {
+            JOptionPane.showInputDialog(null, "skriv ditt K värde");
+        }
+}
